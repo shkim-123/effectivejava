bfc9c32fff7dd92c837cb7efd52c72b0a47a2a9a
SH Kim <82145134+shkim-123@users.noreply.github.com>
2023-06-01 23:06:17 +0900

    객체 생성과 파괴 - 아이템8

---
+++ src/main/java/com/example/effectivejava/ch02/item8/Adult.java
@@ -0,0 +1,9 @@
+package com.example.effectivejava.ch02.item8;
+
+public class Adult {
+    public static void main(String[] args) throws Exception {
+        try (Room myRoom = new Room(7)) {
+            System.out.println("안녕");
+        }
+    }
+}
---
+++ src/main/java/com/example/effectivejava/ch02/item8/Teenager.java
@@ -0,0 +1,9 @@
+package com.example.effectivejava.ch02.item8;
+
+public class Teenager {
+    public static void main(String[] args) {
+        new Room(99);
+        System.out.println("아무렴");
+        System.gc(); // 추가
+    }
+}
